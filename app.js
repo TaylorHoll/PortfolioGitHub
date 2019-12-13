cc2be7cb44e052ad4d2d426a8414420c00f86a47
TaylorHoll <taylorholloway1984@gmail.com>
2019-12-14 00:34:01 +0800

    making

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000, function(){
+//if statement allows running locally as well as on keroku
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
+app.listen(port, function() {
+  console.log("Server started on port 3000");
 });
