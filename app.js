e2a7d70ee2a7374181afe4e8edb36802b7791b48
TaylorHoll <taylorholloway1984@gmail.com>
2019-12-19 05:20:51 +0800

    fixed the cellphone heights i hope

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/vendor"));
-app.use(express.static(__dirname + "/mail"));
+
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/index.html");
