f27b8e150acb17d3001e85bc2624c40667a5b53d
TaylorHoll <taylorholloway1984@gmail.com>
2019-12-22 17:52:22 +0800

    Added force HTTPS

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 //jshint esversion:6
 
+
+const sslRedirect = require('heroku-ssl-redirect');
 const express = require("express");
 const bodyParser = require("body-parser");
 
 
-
 const app = express();
+
+
+app.use(sslRedirect());
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
