7485834081e83661ee1782943aacbeda1ecc9c75
TaylorHoll <taylorholloway1984@gmail.com>
2019-12-22 17:18:43 +0800

    Added security meta tagsasdfarsetq5dd

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.get("/", function(req, res){
 });
 
 app.get("/.well-known/acme-challenge/HCZK0VMw5DUz0bduM0Zn_7fovpAiqxl6QqfBHsnwCkc", function(req, res){
-
+  res.sendFile(__dirname + "/index.html");
 });
 
 //if statement allows running locally as well as on keroku
